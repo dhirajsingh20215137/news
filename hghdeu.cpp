8dc9c916c52f436e38ae6ef9728894e0c87d91d6
ujjainsingh88 <85781890+ujjainsingh88@users.noreply.github.com>
2022-05-21 01:54:09 +0530

    lauda

--- hghdeu.cpp
+++ hghdeu.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main(){
     cout<<"laura lahsan"<<endl;
-<<<<<<< HEAD
-    cout<<"vsb_sachin ";
-=======
+    
+cout<<"sidhe daalo bahinchod"<<endl;
+
+
     if (!(4 < 3))
     cout<<"kapil is chutiyaa tahrki bihar ka lawdasur"<<endl;
->>>>>>> 60658409ac61e28d0e2527c9379d2c7ce6378592
+
 }
