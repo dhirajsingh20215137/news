3f1800ea1b280b50d92806333d1191b29460edf6
ujjainsingh88 <85781890+ujjainsingh88@users.noreply.github.com>
2022-05-21 02:04:33 +0530

    c2

--- hghdeu.cpp
+++ hghdeu.cpp
@@ -10,4 +10,16 @@ cout<<"sidhe daalo bahinchod"<<endl;
     if (!(4 < 3))
     cout<<"kapil is chutiyaa tahrki bihar ka lawdasur"<<endl;
 
+string s[5]={"tera","baap","bol","rha","hu"};
+
+for (int i = 0; i <5; i++)
+{
+   cout<<s[i]<<" ";
 }
+
+
+
+
+}
+
+
