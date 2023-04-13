94303868f5fd38ad5ede6ec66df3894329178af7
Dhiraj Kumar Singh <dirajkumar555888@gmail.com>
2023-04-14 02:40:41 +0530

    third

--- newshub/src/App.js
+++ newshub/src/App.js
@@ -1,10 +1,11 @@
 
 import  React from 'react'
+import { Navbar } from './components/Navbar'
 
 const App= ()=>{
     return (
       <div>
-        App
+        <Navbar/>
       </div>
     )
 }
---
+++ newshub/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export const Navbar =()=>{
+    return <div>
+        <nav class="navbar navbar-expand-lg  bg-dark">
+  <div class="container-fluid"> 
+    <a class="navbar-brand text-white" href="#">NewsHub</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">General</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">Business</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">Entertainment</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">Health</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">Science</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">Sports</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">Technology</a>
+        </li>
+       
+       
+        
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+    </div>
+}
